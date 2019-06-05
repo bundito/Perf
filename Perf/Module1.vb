5fb3b510137f38becf9f6f1b710fc46537162059
Scott Harvey <bundito@gmail.com>
2019-06-05 11:50:13 -0500

    First working version.

---
+++ Perf/Module1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Threading
+Module Module1
+
+
+    Sub Main()
+
+        ' process name, pretty name, executable
+        Dim programs(,) = {
+            {"throttlestop", "ThrottleStop", "C:\ThrottleStop_8.70.6\ThrottleStop.exe"},
+            {"borderlessgaming", "Borderless Gaming", "C:\Program Files (x86)\Borderless Gaming\BorderlessGaming.exe"},
+            {"msiafterburner", "MSI Afterburner", "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe"}
+        }
+
+        Dim i As Integer
+        i = 0
+
+        While i <= programs.GetUpperBound(0)
+            Dim running As Boolean
+            Dim pretty As String
+            Dim exe As String
+
+            running = CheckIfRunning(programs(i, 0))
+            pretty = programs(i, 1)
+            exe = programs(i, 2)
+
+            If running = False Then
+                Dim proc As New System.Diagnostics.Process()
+                proc = Process.Start(programs(i, 2))
+
+                MsgBox(pretty + " went down. Restarting...")
+
+
+            End If
+
+            i += 1
+
+        End While
+
+        Thread.Sleep(1000)
+
+
+
+
+
+        Main()
+
+    End Sub
+
+
+
+    Function CheckIfRunning(processname As String)
+        Dim p() As Process
+        p = Process.GetProcessesByName(processname)
+        If p.Count > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+End Module
+
+
+
---
+++ Perf/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Perf")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Perf")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6c097d38-3cab-4693-acc1-40d2a0057a13")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
